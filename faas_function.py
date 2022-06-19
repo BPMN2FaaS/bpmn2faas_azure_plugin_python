8a92b22c1313ace59c7c7acf9b49f6ebdbe309db
TYueksel <st111539@stud.uni-stuttgart.de>
2022-06-19 23:19:48 +0200

    Implemented deployment package structure

--- faas_function.py
+++ faas_function.py
@@ -6,66 +6,74 @@ from .bpmn import Element
 from .bpmn.startevents.StartEvent import StartEvent
 from .bpmn.tasks.Task import Task
 from .bpmn.tasks.ServiceTask import ServiceTask
+from .bpmn.gateways.Gateway import Gateway
+from .bpmn.endevents.EndEvent import EndEvent
 
 
 class FaaSFunction:
     ns = {'bpmn2': 'http://www.omg.org/spec/BPMN/20100524/MODEL',
           'bpmn2faas': 'http://bpmn2faas'}
 
-    def __init__(self, lane: et.Element, process: et.Element):
+    def __init__(self, lane: et.Element, process: et.Element, endpoints: dict):
         self.lane: et.Element = lane
-        self.name: str = lane.get('name')
+        self.name: str = lane.get('name', default='__init__')
 
         element_ids: [str] = [node_id.text for node_id in lane.findall('./bpmn2:flowNodeRef', self.ns)]
         elements = [process.find(f'./*[@id="{node_id}"]', self.ns) for node_id in element_ids]
 
         self.operations: [Element] = []
         self.services: [str] = []
+
         for element in elements:
-            if element.tag == '{'+self.ns['bpmn2']+'}startEvent':
+            if element.tag == '{' + self.ns['bpmn2'] + '}startEvent':
                 self.start_event: StartEvent = StartEvent(element, process)
-            elif element.tag == '{'+self.ns['bpmn2']+'}task':
-                self.operations.append(Task(element, process))
-            elif element.tag == '{'+self.ns['bpmn2']+'}serviceTask':
+            elif element.tag == '{' + self.ns['bpmn2'] + '}task':
+                task = Task(element, process)
+                self.operations.append(task)
+            elif element.tag == '{' + self.ns['bpmn2'] + '}serviceTask':
                 st = ServiceTask(element, process)
+                st.connection_string = endpoints.get(st.id, '')
                 self.operations.append(st)
                 if st.service not in self.services:
                     self.services.append(st.service)
+            elif element.tag == '{' + self.ns['bpmn2'] + '}exclusiveGateway':
+                gateway = Gateway(element, process, endpoints)
+                if gateway.is_splitting:
+                    self.operations.append(gateway)
+            elif element.tag == '{' + self.ns['bpmn2'] + '}endEvent':
+                self.operations.append(EndEvent(element, process))
         self.operations = self.__get_sequence()
 
+        serviceBus_client_imported = False
         for i in range(0, len(self.services)):
             if self.services[i] == Service.OBJECT_STORAGE:
-                self.services[i] = 's3'
-            elif self.services[i] == Service.QUEUE or self.services[i] == Service.FIFO_QUEUE:
-                self.services[i] = 'sqs'
-            elif self.services[i] == Service.PUBSUB:
-                self.services[i] = 'sns'
+                self.services[i] = 'from azure.storage.blob import BlobServiceClient, ContainerClient'
+            elif self.services[i] == Service.QUEUE:
+                self.services[i] = 'from azure.storage.queue import QueueServiceClient, QueueClient'
+            elif (self.services[i] == Service.FIFO_QUEUE or self.services[i] == Service.PUBSUB) and not serviceBus_client_imported:
+                self.services[i] = 'from azure.servicebus.management import ServiceBusAdministrationClient\n' \
+                                   'from azure.servicebus import ServiceBusClient, ServiceBusSender, ServiceBusReceiver, ServiceBusMessage'
+                serviceBus_client_imported = True
 
         self.modules: [str] = self.__get_modules()
-
-    def to_code(self) -> str:
-        template_folder = os.path.join(os.path.dirname(__file__), 'templates')
-        file_loader = FileSystemLoader(template_folder)
-        env = Environment(loader=file_loader)
-        template = env.get_template('function.py.jinja')
-        data = {'modules': self.modules,
-                'clients': self.services,
-                'handler_name': 'handler',
-                'operations': self.operations,
-                'start_event': self.start_event}
-
-        return template.render(data=data)
 
     def __get_sequence(self) -> [Element]:
         sequence = [self.start_event]
         while self.operations:
             index = 0
             for op in self.operations:
-                if sequence[-1].outgoing[0] == op.id:
+                if type(sequence[-1]) is Gateway and sequence[-1].is_splitting:
+                    if sequence[-1].joining_gateway.outgoing and \
+                            sequence[-1].joining_gateway.outgoing[0].get('id') == op.id:
+                        sequence.append(op)
+                    self.operations.pop(index)
+                    break
+                elif sequence[-1].outgoing[0].get('id') == op.id:
                     sequence.append(op)
                     self.operations.pop(index)
                     break
                 index += 1
+
         return sequence[1:]
 
     def __get_modules(self) -> [str]:
@@ -74,3 +82,16 @@ class FaaSFunction:
             if type(op) is Task and op.module not in modules:
                 modules.append(op.module)
         return modules
+
+    def to_code(self) -> str:
+        template_folder = os.path.join(os.path.dirname(__file__), 'templates')
+        file_loader = FileSystemLoader(template_folder)
+        env = Environment(loader=file_loader)
+        template = env.get_template('function.py.jinja')
+        data = {'modules': self.modules,
+                'clients': self.services,
+                'input_class': self.start_event.input_class,
+                'operations': self.operations,
+                'start_event': self.start_event}
+
+        return template.render(data=data)
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import xml.etree.ElementTree as xml
 import os
 import shutil
 from pathlib import Path
-import subprocess
-import sys
+from jinja2 import Environment, FileSystemLoader
 from .faas_function import FaaSFunction
+from .constants.TriggerTypeConstants import Trigger
 
 
 class Plugin:
@@ -12,13 +12,13 @@ class Plugin:
     ns = {'bpmn2': 'http://www.omg.org/spec/BPMN/20100524/MODEL',
           'bpmn2faas': "http://bpmn2faas"}
 
-    def generate(self, path: str, endpoints: [object]):
+    def generate(self, path: str, endpoints: dict):
+        zip_file = ''
         for filename in os.listdir(path):
             if filename.endswith('.zip.input'):
                 zip_file = filename
-                business_code_path = os.path.join(path, zip_file[:-10])
-                break
 
+        business_code_path = os.path.join(path, zip_file[:-10])
         bpmn_path = os.path.join(path, 'diagram.bpmn')
         root = xml.parse(bpmn_path).getroot()
         pool = root.find('./bpmn2:collaboration/bpmn2:participant', self.ns)
@@ -27,34 +27,93 @@ class Plugin:
         lanes = lane_set.findall('./bpmn2:lane', self.ns)
 
         # create project folder
-        project_name = pool.get('name', default=zip_file[:-10]) + '_aws'
+        project_name = pool.get('name', default=zip_file[:-10]) + '_azure'
         project_path = os.path.join(path, project_name)
         Path(project_path).mkdir(parents=True, exist_ok=True)
 
+        functions: [FaaSFunction] = []
+        requirements = []
+
         for lane in lanes:
-            faas_function = FaaSFunction(lane, process)
+            faas_function = FaaSFunction(lane, process, endpoints)
+            functions.append(faas_function)
 
             function_path = os.path.join(project_path, faas_function.name)
             Path(function_path).mkdir(parents=True, exist_ok=True)
 
-            with open(os.path.join(function_path, faas_function.name+'.py'), "w") as function_file:
+            with open(os.path.join(function_path, faas_function.name+'.py'), 'w') as function_file:
                 function_file.write(faas_function.to_code())
 
-            for module in faas_function.modules:
-                source = os.path.join(business_code_path, module)
-                destination = os.path.join(function_path, module)
-                print(source)
-                shutil.copy2(source, destination)
+            shared_modules_path = os.path.join(project_path, 'shared_code')
+            Path(shared_modules_path).mkdir(parents=True, exist_ok=True)
+
+            for module in os.listdir(business_code_path):
+                if module.endswith('.py'):
+                    source = os.path.join(business_code_path, module)
+                    destination = os.path.join(shared_modules_path, module)
+                    shutil.copy2(source, destination)
 
             requirements_path = os.path.join(business_code_path, faas_function.name, 'requirements.txt')
 
             if os.path.isfile(requirements_path):
-                subprocess.check_call([sys.executable, '-m', 'pip', "install", '-r', requirements_path,
-                                       '--target', os.path.join(function_path, 'package')])
+                with open(requirements_path, 'r') as requirements_file:
+                    requirements += requirements_file.readlines()
+                    if not requirements[-1].endswith('\n'):
+                        requirements[-1] += '\n'
 
-            shutil.make_archive(function_path, 'zip', function_path)
-            shutil.rmtree(function_path)
+            # generate function.json
+            trigger_type = ''
+            schedule = ''
 
+            if faas_function.start_event.trigger_type == Trigger.OBJECT_STORAGE:
+                trigger_type = 'blobTrigger'
+            elif faas_function.start_event.trigger_type == Trigger.TIMER:
+                trigger_type = 'timerTrigger'
+                schedule = faas_function.start_event.schedule
+            elif faas_function.start_event.trigger_type == Trigger.QUEUE:
+                trigger_type = 'queueTrigger'
+            elif faas_function.start_event.trigger_type == Trigger.PUBSUB or \
+                    faas_function.start_event.trigger_type == Trigger.FIFO_QUEUE:
+                trigger_type = 'serviceBusTrigger'
+
+            template_folder = os.path.join(os.path.dirname(__file__), 'templates')
+            file_loader = FileSystemLoader(template_folder)
+            env = Environment(loader=file_loader)
+            template = env.get_template('function.json.jinja')
+            bindings = {'function_name': faas_function.name+'.py',
+                        'trigger_type':  trigger_type,
+                        'schedule':      schedule,
+                        'queue_name':    faas_function.start_event.name,
+                        'connection':    faas_function.start_event.name+'_'+faas_function.start_event.trigger_type.name}
+
+            with open(os.path.join(function_path, 'function.json'), 'w') as function_bindings:
+                function_bindings.write(template.render(data=bindings))
+
+        requirements = set(requirements)
+        with open(os.path.join(project_path, 'requirements.txt'), 'w') as requirements_file:
+            requirements_file.write("".join(requirements))
+
+        connection_strings = []
+        for function in functions:
+            if function.start_event.trigger_type is not Trigger.TIMER:
+                connection_strings.append({'event_source':      function.start_event.name,
+                                           'service_type':      function.start_event.trigger_type.name,
+                                           'connection_string': endpoints[function.start_event.id]})
+
+        # eliminate duplicates
+        connection_strings = list(map(dict, set(tuple(sorted(cs.items())) for cs in connection_strings)))
+
+        template_folder = os.path.join(os.path.dirname(__file__), 'templates')
+        file_loader = FileSystemLoader(template_folder)
+        env = Environment(loader=file_loader)
+        template = env.get_template('local.settings.json.jinja')
+        with open(os.path.join(project_path, 'local.settings.json'), 'w') as local_settings_file:
+            local_settings_file.write(template.render(data=connection_strings))
+
+        host_path = os.path.join(os.path.dirname(__file__), 'resources', 'host.json')
+        funcignore_path = os.path.join(os.path.dirname(__file__), 'resources', 'funcignore_file')
+        shutil.copy2(host_path, os.path.join(project_path, 'host.json'))
+        shutil.copy2(funcignore_path, os.path.join(project_path, '.funcignore'))
         shutil.make_archive(project_path, 'zip', project_path)
         shutil.rmtree(project_path)
 
